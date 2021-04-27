96dc9e01ff95d5b2b8bad46607171e509931ee8b
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-04-27 23:33:30 +0300

    April 27 2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import AppCars from "./components/AppCars";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <AppCars
+            heading={"Fiat Punto I (Тип 176) (1993—1999)"}
+            image={"Fiat_Punto_I.jpg"}
+            description={"В 1993 році в місті Турин було представлене перше покоління Fiat Punto. Автомобіль пропонувався як 3-х і 5-ти дверний хетчбек і кабріолет. В 1995 році Punto був обраний автомобілем року. В 1996 році Punto був найпродаванішим автомобілем в Європі."}
+            />
+        <AppCars heading={"Fiat Punto II (Тип 188) (1999—2012)"}
+                 image={"Fiat_Punto_II.jpg"}
+                 description={"Друге покоління представлене в 1999 році до 100-річчя фірми Fiat. Автомобіль пропонувався як 3-х і 5-ти дверний хетчбек. У 2001 році представлена версія Abarth з ​​двигуном 1,8 16-кл. потужністю 131 к.с. і меншою вагою."}
+        />
+        <AppCars
+            heading={"Fiat Punto III (Тип 199) (2005—2018)"}
+            image={"Fiat_Punto_III.jpg"}
+            description={"Fiat Punto третього покоління отримав назву Fiat Grande Punto, вищу оцінку EuroNCAP за безпеку, удостоєний звання «Найкращий компакт» і нагороди «Золоте кермо» у Німеччині. Punto дебютував у Франкфурті, створений на загальній платформі з моделлю Opel Corsa С, тому ці автомобілі пов'язує до 30% загальних запчастин, а дизайн в стилі Maserati був розроблений ательє Ital Design. Салон створений власної дизайн-студією Fiat."}
+        />
+
     </div>
   );
 }
